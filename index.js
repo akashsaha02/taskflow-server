9c276dc5a421f145487dad8da6155226d4366f41
Akash4056 <asaha8313@gmail.com>
2025-02-21 00:07:57 +0530

    add JWT authentication and middleware for user verification

--- index.js
+++ index.js
@@ -29,6 +29,38 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        console.log('Connecting to the database');
+        const database = client.db("taskflow");
+        const users = database.collection("users");
+        const tasks = database.collection("tasks");
+
+
+        // jwt api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+        });
+
+        // middleware functions
+        const verifyToken = (req, res, next) => {
+            // console.log(req.headers.authorization)
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'Unauthorized request' });
+            }
+
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(403).send({ message: 'Forbidden request' });
+                }
+                req.decoded = decoded;
+                next();
+            });
+        }
+
+
 
     } finally {
         // Ensures that the client will close when you finish/error
