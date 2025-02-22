41a516ee9da31607e8d5c710f449c5249554335e
Akash4056 <asaha8313@gmail.com>
2025-02-22 14:56:36 +0530

    refactor server setup to use modular route handling and improve code organization

--- server.js
+++ server.js
@@ -1,98 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// require('dotenv').config();
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const port = process.env.PORT || 5000;
-// const jwt = require('jsonwebtoken');
-
-
-// // middlewares
-// app.use(cors({
-//     origin: ['http://localhost:5173'],
-//     credentials: true
-// }));
-// app.use(express.json());
-
-// const uri = process.env.MONGO_URI;
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
-
-
-// async function run() {
-//     try {
-//         console.log('Connecting to the database');
-//         const database = client.db("taskflow");
-//         const users = database.collection("users");
-//         const tasks = database.collection("tasks");
-
-
-//         // jwt api
-
-//         app.post('/jwt', async (req, res) => {
-//             const user = req.body;
-//             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-//             res.send({ token })
-//         });
-
-//         // middleware functions
-//         const verifyToken = (req, res, next) => {
-//             // console.log(req.headers.authorization)
-//             if (!req.headers.authorization) {
-//                 return res.status(401).send({ message: 'Unauthorized request' });
-//             }
-
-//             const token = req.headers.authorization.split(' ')[1];
-//             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//                 if (err) {
-//                     return res.status(403).send({ message: 'Forbidden request' });
-//                 }
-//                 req.decoded = decoded;
-//                 next();
-//             });
-//         }
-
-//         // Users Collection API
-
-//         app.post('/users', async (req, res) => {
-//             const user = req.body;
-//             const query = { email: user.email };
-//             const existingUser = await users.findOne(query);
-//             if (existingUser) {
-//                 res.send({ message: 'User already exists', insertedId: existingUser._id });
-//                 return;
-//             }
-//             const result = await users.insertOne(user);
-//             res.json(result);
-//         });
-
-
-
-
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         // await client.close();
-//     }
-// }
-// run().catch(console.dir);
-
-
-// app.get('/', (req, res) => {
-//     res.send('Task Flow Server!');
-// });
-
-
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
-
-
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -102,6 +7,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import authRoutes from "./routes/authRoutes.js"
+
 
 dotenv.config();
 const app = express();
@@ -124,6 +30,7 @@ app.use("/users", userRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
 
+
 // Socket.io Connection
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
@@ -141,6 +48,10 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
 });
 
 export { io };
